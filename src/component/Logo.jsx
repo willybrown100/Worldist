7519c73d85e26b497a0a640385c506b4c0de0fe1
willy brown <williamsgoddey11@gmail.com>
2024-03-15 06:24:07 +0100

    Initial commit

--- src/component/Logo.jsx
+++ src/component/Logo.jsx
@@ -3,8 +3,7 @@ import styles from './Logo.module.css'
 function Logo() {
  return (
    <div className={styles.logoContainer}>
-     <img src="/worldist logo.jpg" alt="WorldWise logo" className={styles.logo} />
-     <h2>worldist</h2>
+     <img src="/worldist logo1.jpg" alt="WorldWise logo" className={styles.logo} />
    </div>
  );
 }
